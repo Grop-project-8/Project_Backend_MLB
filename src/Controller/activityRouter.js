c2a3fd693556478ae5a566b54e04fa9521467550
Tanakrit Sonsue <tanakrit.sonsue@gmail.com>
2023-10-25 19:45:58 +0700

    Update BE

--- src/Controller/activityRouter.js
+++ src/Controller/activityRouter.js
@@ -2,8 +2,6 @@
 import jwt from 'jsonwebtoken';
 import User from "../Models/usermodel.js";
 
-
-
 // Create Post
 export const createPost = async (req, res) => {
   try {
@@ -11,10 +9,19 @@ export const createPost = async (req, res) => {
     const token = req.cookies.token;
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     const username = decodedToken.user.username;
-
+    
     const existingUser = await User.findOneAndUpdate({ username: username });
     if (!existingUser) {
       return res.status(404).send("User not found");
+    }
+    if (activityname.length < 2 || activitytype.length < 2) {
+      return res.status(400).send("Activity name and type must be at least 2 characters long");
+    }
+    if (duration <= 1) {
+      return res.status(400).send("Duration must be greater than 0");
+    }
+    if (detail.length < 2) {
+      return res.status(400).send('Please enter details')
     }
 
     existingUser.activity.push({ activitytype,activityname, detail,duration,createdAt });
@@ -26,68 +33,47 @@ export const createPost = async (req, res) => {
   }
 };
 
-// Edit Post
-/* export const editPost = async (req,res) => {
-    try {
-        const id = req.body.id
-        console.log(id);
-        const token = req.cookies.token;
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        const username = decodedToken.user.username;
-
-        const existingUser = await User.findOne({ username: username });
-        if (!existingUser) {
-          return res.status(404).send("User not found");
-      }
-
-        const updated = await User.findOneAndUpdate({ _id : id }).exec();
-        res.send(updated)
-    } catch (error) {
-        res.status(500).send('Server error: ' + error.message)
-    }
-} */
-// Edit Post
-export const editPost = async (req, res) => {
+// Delete
+export const deletePost = async (req, res) => {
   try {
-    const { id, updatedActivityType, updatedActivityName, updatedDetail, updatedDuration  } = req.body;
+    const id = req.body.id; // รับ ID ของกิจกรรมที่ต้องการลบจาก req.body
+
+    // ค้นหาผู้ใช้โดยใช้ ID หรืออื่น ๆ ที่คุณต้องการใช้ในการตรวจสอบสิทธิ์การลบ
     const token = req.cookies.token;
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     const username = decodedToken.user.username;
 
-    // ค้นหาผู้ใช้โดยใช้ชื่อผู้ใช้
     const existingUser = await User.findOne({ username: username });
+
     if (!existingUser) {
       return res.status(404).send("User not found");
     }
 
-    // ค้นหากิจกรรมที่ต้องการแก้ไขในรายการกิจกรรมของผู้ใช้
+    // ค้นหากิจกรรมที่ต้องการลบในรายการกิจกรรมของผู้ใช้
     const activityIndex = existingUser.activity.findIndex((act) => act._id == id);
 
     if (activityIndex === -1) {
       return res.status(404).send("Activity not found");
     }
 
-    // อัปเดตข้อมูลกิจกรรม
-    existingUser.activity[activityIndex].activitytype = updatedActivityType;
-    existingUser.activity[activityIndex].activityname = updatedActivityName;
-    existingUser.activity[activityIndex].detail = updatedDetail;
-    existingUser.activity[activityIndex].duration = updatedDuration;
-    existingUser.activity[activityIndex].createdAt = Date.now();
+    // ลบกิจกรรมจากรายการกิจกรรมของผู้ใช้
+    existingUser.activity.splice(activityIndex, 1);
 
-
+    // บันทึกการเปลี่ยนแปลงในฐานข้อมูลโดยใช้ Mongoose
     await existingUser.save();
 
-    res.send("Activity updated successfully");
+    res.send("Activity deleted successfully");
   } catch (error) {
     res.status(500).send("Server error: " + error.message);
   }
-};
+}
 
-// Delete
-export const deletePost = async (req, res) => {
+
+export const editPost = async (req, res) => {
+/*   console.log('body', req.body); */
+
   try {
-      const id = req.body.id; // รับ ID ของกิจกรรมที่ต้องการลบจากคำขอ
-      console.log(id)
+    const { id, updatedDetail, updatedDuration  } = req.body;
 
       // ค้นหาผู้ใช้โดยใช้ ID หรืออื่น ๆ ที่คุณต้องการใช้ในการตรวจสอบสิทธิ์การลบ
       const token = req.cookies.token;
@@ -97,29 +83,27 @@ export const deletePost = async (req, res) => {
       const existingUser = await User.findOne({ username: username });
 
       if (!existingUser) {
-          return res.status(404).send("User not found");
-      }
+        return res.status(404).send("User not found");
+    }
+    if (updatedDetail.length < 2) {
+      return res.status(400).send("Activity name and type must be at least 2 characters long");
+    }
+    if (duration <= 1) {
+      return res.status(400).send("Duration must be greater than 0");
+    }
 
-      // ค้นหากิจกรรมที่ต้องการลบในรายการกิจกรรมของผู้ใช้
-      const activityIndex = existingUser.activity.findIndex(act => act._id == id);
-
-      if (activityIndex === -1) {
-          return res.status(404).send("Activity not found");
-      }
-
-      // ลบกิจกรรมจากรายการกิจกรรมของผู้ใช้
-      existingUser.activity.splice(activityIndex, 1);
-
-      // บันทึกการเปลี่ยนแปลงในฐานข้อมูลโดยใช้ Mongoose
-      await existingUser.save();
-
-      res.send("Activity deleted successfully");
-
+     const update = await User.findOneAndUpdate({"activity._id": id },
+      {
+        $set: {
+          "activity.$.detail": updatedDetail,
+          "activity.$.duration": updatedDuration,
+          "activity.$.createdAt": Date.now()
+        }
+      },
+      { new: true }
+    )
+      res.send(update)
   } catch (error) {
-      res.status(500).send("Server error: " + error.message);
+    console.log(error);
   }
 }
-
-
-
-
