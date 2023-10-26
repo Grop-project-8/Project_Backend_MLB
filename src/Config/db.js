6ab96f0454bf2f31be5274b08d84d5c96ae14014
Voraphat <mr.voraphat@gmail.com>
2023-10-26 09:15:04 +0700

    bank

--- src/Config/db.js
+++ src/Config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE, {
+        await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
