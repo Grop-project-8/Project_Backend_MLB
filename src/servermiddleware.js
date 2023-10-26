039e88abe38d4ef91406183dd8250dec2603be23
Voraphat <mr.voraphat@gmail.com>
2023-10-26 09:46:11 +0700

    core

--- src/servermiddleware.js
+++ src/servermiddleware.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import "dotenv/config";
 
 const corsOptions = {
-    origin: 'https://mlbproject-aavjuooi9-mlb.vercel.app', 
+    origin: 'https://project-mlb.vercel.app', 
     methods: "GET,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 204, 
